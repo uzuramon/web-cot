f22ae79036033f0aeb3d1e4925873b4dce827b1e
uzuramon <uzuramon@gmail.com>
2012-12-09 20:41:40 +0900

    版権差し替え

--- js/webcot-messages.js
+++ js/webcot-messages.js
@@ -10,31 +10,15 @@ var messages = new Array();
  * mesage: ふきだしに表示するメッセージ
  * char: 画像番号
  */
-messages[0] = [{char: "samon", message: "団子もらったぞー！", imgNumber: 3},
-               {char: "sannosuke", message: "おー！茶入れてくる！", imgNumber: 3},
-               {char: "sakube", message: "お前は行くな！", imgNumber: 2}];
-messages[1] = [{char: "samon", message: "会計委員会室はどっちだー！", imgNumber: 2},
-               {char: "sakube", message: "待ってろ、今連れて行く。", imgNumber: 2},
-               {char: "sannosuke", message: "裏山どこだっけ？", imgNumber: 1},
-               {char: "sakube", message: "……四年の先輩呼んでくるから動くなよ？", imgNumber: 2}];
-messages[2] = [{char: "sannosuke", message: "さくべーさくべー", imgNumber: 3},
-               {char: "samon", message: "さくべー！", imgNumber: 3},
-               {char: "sakube", message: "なんだよ", imgNumber: 1},
-               {char: "sannosuke", message: "呼んでみただけ", imgNumber: 3},
-               {char: "samon", message: "左に同じく！", imgNumber: 3}];
-messages[3] = [{char: "samon", message: "それにしても寒いなー", imgNumber: 1},
-               {char: "sannosuke", message: "用具委員は湯たんぽ作らないの？", imgNumber: 1},
-               {char: "sakube", message: "予算よこせ", imgNumber: 2}];
-messages[4] = [{char: "sannosuke", message: "今日も裏々々山までマラソンしてきた……", imgNumber: 1},
-               {char: "samon", message: "今日で三徹だな！", imgNumber: 1},
-               {char: "sakube", message: "……お前らも大変だな", imgNumber: 1}];
-messages[5] = [{char: "sakube", message: "しょうぶだー！", imgNumber: 3},
-               {char: "samon", message: "ぎんぎーん！", imgNumber: 3},
-               {char: "sannosuke", message: "いけいけどんどーん！", imgNumber: 3}];
-messages[6] = [{char: "sakube", message: "疲れたらちゃんと休めよ？", imgNumber: 2},
-               {char: "samon", message: "……池で寝なくていいように祈っててくれ！", imgNumber: 1},
-               {char: "sannosuke", message: "……七松先輩がバレーボールをはじめないように祈っててよ", imgNumber: 1}];
-messages[7] = [{char: "samon", message: "明日はどっちだー！", imgNumber: 2},
-               {char: "sannosuke", message: "こっちかなー", imgNumber: 3},
-               {char: "sakube", message: "探すのはかまわねえが、外に行くなら声かけろよ", imgNumber: 2}];
-
+messages[0] = [{char: "samon", message: "あめんぼあかいな", imgNumber: 3},
+               {char: "sannosuke", message: "あいうえお", imgNumber: 3},
+               {char: "sakube", message: "うきもにこえびもおよいでる", imgNumber: 2}];
+messages[1] = [{char: "samon", message: "かきのきくりのき", imgNumber: 2},
+               {char: "sakube", message: "かきくけこ", imgNumber: 2},
+               {char: "sannosuke", message: "きつつきこつこつ", imgNumber: 1},
+               {char: "sakube", message: "かれけやき", imgNumber: 2}];
+messages[2] = [{char: "sannosuke", message: "ささげにすをかけ", imgNumber: 3},
+               {char: "samon", message: "さしすせそ", imgNumber: 3},
+               {char: "sakube", message: "そのうおあさせで", imgNumber: 1},
+               {char: "sannosuke", message: "さしました", imgNumber: 3},
+               {char: "samon", message: "らいちょうさむかろらりるれろ", imgNumber: 3}];
